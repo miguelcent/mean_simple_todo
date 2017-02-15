692e4bc96dc1c290267b466255813a44b4000d7e
miguelcent <miguelcent@gmail.com>
2017-02-15 15:12:50 +0100

    services and controllers adding (step3)

---
+++ public/js/services/todos.js
@@ -0,0 +1,23 @@
+// js/service/todos.js
+angular.module('todoService', [])
+	//super simple service
+	//each function returns a promise object
+	.factory('Todos', function($http) {
+		return {
+			get : function() {
+				return $http.get('api/todos');
+			},
+			create : function(todoDate) {
+				return $http.post('/api/todos', todoData);
+			},
+			delete : function(id) {
+				return $http.delete('/api/todos' + id);
+			}
+		}
+	});
+
+/*
+	That’s all there is to it. We have defined our service using 
+	.factory with three different functions. 
+	get, create and delete will all return promise objects that we can use in our controller.
+*/
